d07ad4d9f16ca00109d276fa4d7affd2f45c8037
caiofabiogomes <caiofabio250@gmail.com>
2025-03-09 12:38:17 -0300

    add SdkModule

--- TechChallenge.SDK/SdkModule.cs
+++ TechChallenge.SDK/SdkModule.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechChallenge.SDK.Persistence;
 
 namespace TechChallenge.SDK
 {
-    internal class SdkModule
+    public static class SdkModule
     {
+        public static IServiceCollection AddSdkModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ContactsDBContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IContactRepository, ContactRepository>();
+            return services;
+        }
     }
 }
