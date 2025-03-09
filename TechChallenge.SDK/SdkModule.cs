d599fcbbcf9e9a04003ccd78575495bdd3916633
caiofabiogomes <caiofabio250@gmail.com>
2025-03-09 12:55:01 -0300

    SdkModule

--- TechChallenge.SDK/SdkModule.cs
+++ TechChallenge.SDK/SdkModule.cs
@@ -7,13 +7,24 @@ namespace TechChallenge.SDK
 {
     public static class SdkModule
     {
-        public static IServiceCollection AddSdkModule(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection RegisterSdkModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                    .AddPersistence(configuration)
+                    .RegisterRepositories();
+        }
+
+        private static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
-            services.AddDbContext<ContactsDBContext>(options =>
-            {
-                options.UseSqlServer(connectionString);
-            });
+
+            services.AddDbContext<ContactsDBContext>(options => options.UseSqlServer(connectionString));
+
+            return services;
+        }
+
+        private static IServiceCollection RegisterRepositories(this IServiceCollection services)
+        {
             services.AddScoped<IContactRepository, ContactRepository>();
             return services;
         }
